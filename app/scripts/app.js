82ce5fd6215f425c3050185c609ad8f592489e87
jmt <jeanmichel53@hotmail.com>
2017-01-25 22:24:13 +0100

    feat(angular): first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nodeProjectApp
+ * @description
+ * # nodeProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nodeProjectApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodeProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nodeProjectApp
+ */
+angular.module('nodeProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
