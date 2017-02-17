9fca66254be04bf016c798e54b9d5222cf3ed4b4
jmt <jeanmichel53@hotmail.com>
2017-02-17 21:21:36 +0100

    feat(auth): redirect if no login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,19 +28,36 @@ angular
       .when('/create-project', {
         templateUrl: 'views/create-project.html',
         controller: 'CreateProjectCtrl',
-        controllerAs: 'createProject'
+        controllerAs: 'createProject',
+				resolve: checkLogin()
       })
       .when('/projects', {
         templateUrl: 'views/projects.html',
         controller: 'ProjectsCtrl',
-        controllerAs: 'projects'
+        controllerAs: 'projects',
+				resolve: checkLogin()
       })
       .when('/project/:projectId', {
         templateUrl: 'views/project.html',
         controller: 'ProjectCtrl',
-        controllerAs: 'project'
+        controllerAs: 'project',
+				resolve: checkLogin()
       })
       .otherwise({
         redirectTo: '/'
       });
   });
+
+function checkLogin() {
+	return {
+		login: ['$q', '$location', '$cookies', function ($q, $location, $cookies) {
+			var loginCookie = $cookies.get('login');
+
+			if (!loginCookie) {
+				$q.reject();
+				$location.path('/');
+			}
+			$q.resolve(loginCookie);
+		}]
+	}
+}
